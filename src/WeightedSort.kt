04f955a9dd6d1ffabd66bdc2ee7232b862446fa7
Giovanni Andreuzza <giovanniandreuzza@MBP-di-Giovanni.fritz.box>
2019-06-27 00:05:08 +0200

    For the moment it seems to work only with array bigger than ~10000 number size. Have to test it with random lists

--- src/WeightedSort.kt
+++ src/WeightedSort.kt
@@ -1,5 +1,35 @@
 fun main(args: Array<String>) {
+    val a = mutableListOf<Int>()
 
-    println("Ciao")
+    var mp = 0.0
+    var sum = 0.0
 
+    for (i in 1..1000000) {
+        a.add(i)
+    }
+
+    val indexOfItem = weightedSort(a.toTypedArray(), getMp(a.toTypedArray()), 100)
+
+    if (indexOfItem < a.size)
+        println("Element at $indexOfItem -> ${a[indexOfItem]}")
+    else
+        println("i > size")
+
+}
+
+fun getMp(array: Array<Int>): Double {
+    var mp = 0.0
+    var sum = 0.0
+
+    for (i in 0 until array.size) {
+        val p: Double = i.toDouble() / array[i]
+        mp += array[i] * p
+        sum += p
+    }
+
+    return mp / sum
+}
+
+fun weightedSort(array: Array<Int>, mp: Double, x: Int): Int {
+    return ((x * mp) / (mp + x)).toInt()
 }
